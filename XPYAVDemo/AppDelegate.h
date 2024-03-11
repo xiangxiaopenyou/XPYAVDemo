efda1626a932a25586e714f52bf36b55f3fe88c9
xiangxiaopenyou <linpingxiang@faceunity.com>
2024-03-11 14:02:24 +0800

    Initial commit

---
+++ XPYAVDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  XPYAVDemo
+//
+//  Created by MoMo on 2024/3/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XPYAVDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  XPYAVDemo
+//
+//  Created by MoMo on 2024/3/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
